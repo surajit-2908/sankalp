b8853f96b3cfc7793715318ad5775671df1d0a61
Surajit <surajit.sil@ogmaconceptions.com>
2022-11-28 20:17:45 +0530

    sidebar

--- resources/views/includes/frontend/sidebar.blade.php
+++ resources/views/includes/frontend/sidebar.blade.php
@@ -8,15 +8,16 @@
                     <span class="nav-header-primary"><i class="fa fa-calendar"></i> Product</span>
                 </a>
                 <ul class="nav nav-list collapse" id="submenu1">
+                    <li><a href="javascript:void(0)">Accessories</a></li>
                     <li><a href="javascript:void(0)">Filter Housing</a></li>
                     <li><a href="javascript:void(0)">Filter Consumable</a></li>
                 </ul>
             </li>
             <li>
-                <a class="accordion-heading" data-toggle="collapse" data-target="#submenu4">
-                    <span class="nav-header-primary"><i class="fa fa-calendar"></i> Accessories</span>
+                <a class="accordion-heading" data-toggle="collapse" data-target="#submenu5">
+                    <span class="nav-header-primary"><i class="fa fa-paperclip"></i> Accessories</span>
                 </a>
-                <ul class="nav nav-list collapse" id="submenu4">
+                <ul class="nav nav-list collapse" id="submenu5">
                     <li><a href="javascript:void(0)">“O” rings</a></li>
                     <li><a href="javascript:void(0)">Filter Pads</a></li>
                     <li><a href="javascript:void(0)">Gasket</a></li>
@@ -28,6 +29,7 @@
                     <span class="nav-header-primary"><i class="fa fa-list-alt"></i> Services</span>
                 </a>
                 <ul class="nav nav-list collapse" id="submenu2">
+                    <li><a href="javascript:void(0)">Accessories</a></li>
                     <li><a href="javascript:void(0)">Integrity Testing</a></li>
                     <li><a href="javascript:void(0)">Retrofit Filters</a></li>
                     <li><a href="javascript:void(0)">Filter Servicing</a></li>
@@ -51,6 +53,7 @@
                     <span class="nav-header-primary"><i class="fa fa-handshake-o"></i> Support</span>
                 </a>
                 <ul class="nav nav-list collapse" id="submenu4">
+                    <li><a href="javascript:void(0)">About Us</a></li>
                     <li><a href="{{ route('enquiry') }}">Contact Us</a></li>
                 </ul>
             </li>
